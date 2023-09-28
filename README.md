215cc47cd89734f36c8b507c5cd1b39b939d033a
Apeksha <apekshawarnakulasooriya98@gmail.com>
2023-09-28 19:57:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# avenger_recognition
+ This app enables user to read the characters of "Avengers" movie using Machine Learning technology
---
+++ android/app/src/main/kotlin/com/example/avenger_recognition/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.avenger_recognition
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
